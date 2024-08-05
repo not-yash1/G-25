16dc924471d3722ffe7280bc73e4f49705f8fb94
not-soo-cool <yashvendrasengar1111@gmail.com>
2024-08-06 01:21:03 +0530

    Aug 05

--- Aug_05/server.js
+++ Aug_05/server.js
@@ -1,9 +1,14 @@
 
-import express from "express";
-import users from "./data.json" assert { type: "json" };
+// import express from "express";
+// import users from './data.json' assert { type: 'json' };
+
+const express = require("express");
+const users = require('./data.json')
 
 const app = express();
 const PORT = 4000
+
+app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     console.log(`Server is running at ${req.protocol}://${req.hostname}${PORT}/${req.path}`);
@@ -22,20 +27,27 @@ app.get("/users", (req, res) => {
     res.send(users)
 })
 
-// app.get("/api/users", (req, res) => {
-//     const html = `
-//     <ul>
-//         ${users.map(user => `<li>${user.first_name}</li>`).join('')}
-//     </ul>
-//     `
-//     res.send(html);
-// })
+app.get("/api/users", (req, res) => {
+    const html = `
+    <ul>
+        ${users.map(user => `<li>${user.first_name}</li>`).join('')}
+    </ul>
+    `
+    res.send(html);
+})
 
-// app.get("/users/:id", (req, res) => {
-//     const id = req.params.id
-//     const user = users.find(user => user.id == id)
-//     res.send(user)
-// })
+app.get("/users/:id", (req, res) => {
+    const id = req.params.id
+    const user = users.find(user => user.id == id)
+    res.send(user)
+})
+
+app.post("/api/users", (req, res) => {
+    const body = req.body;
+    console.log(body);
+    users.push({...body, id: users.length + 1})
+    res.send("User added successfully...")
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
@@ -46,12 +58,7 @@ app.listen(PORT, () => {
 
 
 
-// app.post("/api/users", (req, res) => {
-//     const body = req.body;
-//     console.log(body);
-//     users.push({...body, id: users.length + 1})
-//     res.send("User added successfully...")
-// })
+
 
 // app.delete("/users/:id", (req, res) => {
 //     const id = req.params.id
